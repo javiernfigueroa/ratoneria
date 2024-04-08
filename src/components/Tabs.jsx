b0d8bea9fd4362a6cbb4c3b2283f9e308b156120
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-04-08 17:34:49 -0400

    fix local

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,30 +1,122 @@
 import { useState, useEffect } from 'react';
+import Modal from './Modal.jsx';
+import { useForm } from 'react-hook-form';
 import axios from 'axios';
 //coment/
-function Tabs({ localId }) {
+function Tabs({ localId, view }) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const [consumedData, setConsumedData] = useState();
   const [reviewData, setReviewData] = useState();
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setModalContent] = useState(null);
+  
+  const handleOpenModal = (content) => {
+    setModalContent(content);
+    setShowModal(true);
+  };
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:3000/api/v1/consumed/${localId}`,
+      );
+      setConsumedData(response.data.consumed);
+      //console.log(consumedData)
 
+      const resReview = await axios.get(
+        `http://localhost:3000/api/v1/reviews/${localId}`,
+      );
+      setReviewData(resReview.data.reviews);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(
-          `http://localhost:3000/api/v1/consumed/${localId}`,
-        );
-        setConsumedData(response.data.consumed);
-        //console.log(consumedData)
-
-        const resReview = await axios.get(
-          `http://localhost:3000/api/v1/reviews/${localId}`,
-        );
-        setReviewData(resReview.data.reviews);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
     fetchData();
-  }, [localId]);
+  }, [0]);
+  const isSubmit = (data) => {
+    if(data.comment === '') {
+      sendPrice(data);
+    }else {
+      sendComment(data);
+    }
+    
+  };
+
+  const sendComment = async (data) => {
+    try {
+      const userId = localStorage.getItem('id');
+      const token = localStorage.getItem('token');
+      const comentario = {
+        user_id: userId,
+        shop_id: localId,
+        comment: data.comment,
+        calification: null,
+      };
+
+      // Configurar el encabezado de la solicitud con el token
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // Realizar la solicitud POST al backend con el encabezado configurado
+      await axios.post(
+        'http://localhost:3000/api/v1/reviews',
+        comentario,
+        config,
+      );
+
+      // Opcional: Puedes manejar alguna acción después de enviar los comentarios, como cerrar el modal
+      setShowModal(false);
+    } catch (error) {
+      console.error('Error al enviar comentario:', error);
+      // Manejar el error según tus necesidades
+    }
+    fetchData();
+  };
+
+  const sendPrice = async (data) => {
+    try {
+      const userId = localStorage.getItem('id');
+      const token = localStorage.getItem('token');
+      const producto = {
+        user_id: userId,
+        shop_id: localId,
+        product: data.product,
+        price_paid: parseInt(data.price_paid),
+      };
+
+      console.log(producto);
+
+      // Configurar el encabezado de la solicitud con el token
+      const config = {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      };
+
+      // Realizar la solicitud POST al backend con el encabezado configurado
+      await axios.post(
+        'http://localhost:3000/api/v1/consumed',
+        producto,
+        config,
+      );
+
+      // Opcional: Puedes manejar alguna acción después de enviar los comentarios, como cerrar el modal
+      setShowModal(false);
+      
+    } catch (error) {
+      console.error('Error al enviar comentario:', error);
+      // Manejar el error según tus necesidades
+    }
+    fetchData();
+  };
 
   const tabsData = [
     {
@@ -71,14 +163,89 @@ function Tabs({ localId }) {
         })}
       </div>
       <div className="py-4 ml-4">
-        <ul className="overflow-y-scroll h-28 ">
+      {view !== 'gallery' ?
+        <ul className="overflow-y-scroll h-40">
           {tabsData[activeTabIndex].content || 'Loading...'}
-        </ul>
-        {/* {tabsData[activeTabIndex].label === "Precios"
-        ? <Button>Agregar</Button>
-        : <Button>Comentar</Button>}   */}
+        </ul>: <ul className="overflow-y-scroll h-10">
+          {tabsData[activeTabIndex].content || 'Loading...'}
+        </ul>}
+        {view !== 'gallery' ?
+     tabsData[activeTabIndex].label === "Precios"
+        ? <button className="bg-porange p-2 rounded-sm mt-4" onClick={() => handleOpenModal('Precios')}>Agregar</button>
+        : <button className="bg-porange p-2 rounded-sm mt-4" onClick={() => handleOpenModal('Comentarios')}>Comentar</button>   : null} 
       </div>
-      {/* <Modal/> */}
+      {showModal && (
+        <Modal setShowModal={setShowModal}>
+          {modalContent === 'Comentarios' ? (
+            <>
+              <form
+                onSubmit={handleSubmit(isSubmit)}
+                className="flex flex-col gap-5"
+              >
+                <h3 className="text-white p-0 m-0">Deja tu comentario</h3>
+                <input
+                  className="p-1 text-black"
+                  defaultValue=""
+                  placeholder="Ingresa tu comentario"
+                  {...register('comment', { required: true })}
+                />
+                {errors.comment && (
+                  <span className="text-white bg-red-500 p-2">
+                    Debes ingresar un comentario
+                  </span>
+                )}
+                {/* Verificar si el usuario está logueado antes de mostrar el botón de enviar */}
+                {localStorage.getItem('id') && localStorage.getItem('token') ? (
+                  <input type="submit" className=" bg-porange rounded-lg" />
+                ) : (
+                  <span className="text-white bg-red-500 p-2">
+                    Para enviar un comentario debes estar logueado.
+                  </span>
+                )}
+              </form>
+            </>
+          ) : (
+            <>
+              <form
+                onSubmit={handleSubmit(isSubmit)}
+                className="flex flex-col gap-5"
+              >
+                <h3 className="text-white p-0 m-0">Crea tu producto</h3>
+                <input
+                  className="p-1 text-black"
+                  defaultValue=""
+                  placeholder="Ingresa tu producto"
+                  {...register('product', { required: true })}
+                />
+                {errors.product && (
+                  <span className="text-white bg-red-500 p-2">
+                    Debes ingresar un producto
+                  </span>
+                )}
+                <input
+                  className="p-1 text-black"
+                  defaultValue=""
+                  placeholder="Ingresa el precio"
+                  {...register('price_paid', { required: true })}
+                />
+                {errors.price_paid && (
+                  <span className="text-white bg-red-500 p-2">
+                    Debes ingresar un precio
+                  </span>
+                )}
+                {/* Verificar si el usuario está logueado antes de mostrar el botón de enviar */}
+                {localStorage.getItem('id') && localStorage.getItem('token') ? (
+                  <input type="submit" className=" bg-porange rounded-lg" />
+                ) : (
+                  <span className="text-white bg-red-500 p-2">
+                    Para agregar un producto debes estar logueado.
+                  </span>
+                )}
+              </form>
+            </>
+          )}
+        </Modal>
+      )}
     </div>
   );
 }
--- src/views/Local.jsx
+++ src/views/Local.jsx
@@ -5,23 +5,8 @@ import Chat from '../components/Chat.jsx';
 import SocialLinks from '../components/ui/Rrss.jsx';
 import Tabs from '../components/Tabs.jsx';
 import Star from '../components/Star.jsx';
-import Button from '../components/ui/Button.jsx';
-import Modal from '../components/Modal.jsx';
-import { useForm } from 'react-hook-form';
-
-import axios from 'axios';
 
 function Local() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm();
-
-  const isSubmit = (data) => {
-    enviarComentario(data);
-    enviarPrecios(data);
-  };
 
   const { cards } = useContext(AppContext);
   const { id } = useParams();
@@ -29,8 +14,6 @@ function Local() {
     const storedLocal = localStorage.getItem('local');
     return storedLocal ? JSON.parse(storedLocal) : null;
   });
-  const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState(null);
 
   useEffect(() => {
     const foundLocal = cards.find(
@@ -39,83 +22,10 @@ function Local() {
     setLocal(foundLocal);
     //localStorage.setItem('local', JSON.stringify(foundLocal));
   }, [id, cards]);
-
-  const handleOpenModal = (content) => {
-    setModalContent(content);
-    setShowModal(true);
-  };
-
+ 
   if (!local) {
     return <div>Loading...</div>;
   }
-
-  const enviarComentario = async (data) => {
-    try {
-      const userId = localStorage.getItem('id');
-      const token = localStorage.getItem('token');
-      const comentario = {
-        user_id: userId,
-        shop_id: id,
-        comment: data.comment,
-        calification: null,
-      };
-
-      // Configurar el encabezado de la solicitud con el token
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-
-      // Realizar la solicitud POST al backend con el encabezado configurado
-      await axios.post(
-        'http://localhost:3000/api/v1/reviews',
-        comentario,
-        config,
-      );
-
-      // Opcional: Puedes manejar alguna acción después de enviar los comentarios, como cerrar el modal
-      setShowModal(false);
-    } catch (error) {
-      console.error('Error al enviar comentario:', error);
-      // Manejar el error según tus necesidades
-    }
-  };
-
-  const enviarPrecios = async (data) => {
-    try {
-      const userId = localStorage.getItem('id');
-      const token = localStorage.getItem('token');
-      const producto = {
-        user_id: userId,
-        shop_id: id,
-        product: data.product,
-        price_paid: parseInt(data.price_paid),
-      };
-
-      console.log(producto);
-
-      // Configurar el encabezado de la solicitud con el token
-      const config = {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      };
-
-      // Realizar la solicitud POST al backend con el encabezado configurado
-      await axios.post(
-        'http://localhost:3000/api/v1/consumed',
-        producto,
-        config,
-      );
-
-      // Opcional: Puedes manejar alguna acción después de enviar los comentarios, como cerrar el modal
-      setShowModal(false);
-    } catch (error) {
-      console.error('Error al enviar comentario:', error);
-      // Manejar el error según tus necesidades
-    }
-  };
   return (
     <div className="lg:w-[1024px] xl:w-[1280px] w-full mx-auto relative mb-10 mt-10">
       <div className="bg-pdark-grey inline-block w-full h-full align-top mx-auto px-10 py-10">
@@ -134,15 +44,7 @@ function Local() {
           </div>
           <Star paramRating={local.rating}></Star>
           <div className="mt-10 w-11/12 md:w-5/6 ">
-            <Tabs localId={id} />
-            <div className="flex gap-3">
-              <Button onClick={() => handleOpenModal('Comentarios')}>
-                Comentar
-              </Button>
-              <Button onClick={() => handleOpenModal('Precios')}>
-                Agregar Producto
-              </Button>
-            </div>
+            <Tabs localId={id} view="Shop" />
           </div>
         </div>
         <div className="flex flex-col w-full md:w-1/2 mt-5 md:mt-0 ">
@@ -162,78 +64,6 @@ function Local() {
           </div>
         </div>
       </div>
-      {showModal && (
-        <Modal setShowModal={setShowModal}>
-          {modalContent === 'Comentarios' ? (
-            <>
-              <form
-                onSubmit={handleSubmit(isSubmit)}
-                className="flex flex-col gap-5"
-              >
-                <h3 className="text-white p-0 m-0">Deja tu comentario</h3>
-                <input
-                  className="p-1"
-                  defaultValue=""
-                  placeholder="Ingresa tu comentario"
-                  {...register('comment', { required: true })}
-                />
-                {errors.comment && (
-                  <span className="text-white bg-red-500 p-2">
-                    Debes ingresar un comentario
-                  </span>
-                )}
-                {/* Verificar si el usuario está logueado antes de mostrar el botón de enviar */}
-                {localStorage.getItem('id') && localStorage.getItem('token') ? (
-                  <input type="submit" className=" bg-porange rounded-lg" />
-                ) : (
-                  <span className="text-white bg-red-500 p-2">
-                    Para enviar un comentario debes estar logueado.
-                  </span>
-                )}
-              </form>
-            </>
-          ) : (
-            <>
-              <form
-                onSubmit={handleSubmit(isSubmit)}
-                className="flex flex-col gap-5"
-              >
-                <h3 className="text-white p-0 m-0">Crea tu producto</h3>
-                <input
-                  className="p-1"
-                  defaultValue=""
-                  placeholder="Ingresa tu producto"
-                  {...register('product', { required: true })}
-                />
-                {errors.product && (
-                  <span className="text-white bg-red-500 p-2">
-                    Debes ingresar un producto
-                  </span>
-                )}
-                <input
-                  className="p-1"
-                  defaultValue=""
-                  placeholder="Ingresa el precio "
-                  {...register('price_paid', { required: true })}
-                />
-                {errors.price_paid && (
-                  <span className="text-white bg-red-500 p-2">
-                    Debes ingresar un precio
-                  </span>
-                )}
-                {/* Verificar si el usuario está logueado antes de mostrar el botón de enviar */}
-                {localStorage.getItem('id') && localStorage.getItem('token') ? (
-                  <input type="submit" className=" bg-porange rounded-lg" />
-                ) : (
-                  <span className="text-white bg-red-500 p-2">
-                    Para agregar un producto debes estar logueado.
-                  </span>
-                )}
-              </form>
-            </>
-          )}
-        </Modal>
-      )}
     </div>
   );
 }
