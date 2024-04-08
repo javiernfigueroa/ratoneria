bcd5a4233780583487185924174375fdc4f2cf04
samkee1001~ <samkeetangcl@gmail.com>
2024-04-08 13:20:24 -0400

    Fix local

--- src/views/Local.jsx
+++ src/views/Local.jsx
@@ -117,15 +117,15 @@ function Local() {
     }
   };
   return (
-    <div className="lg:w-[1280px] w-full  mx-auto relative mb-10 mt-10">
-      <div className="bg-pdark-grey inline-block w-full h-full align-top  mx-auto px-10 py-10 ">
-        <h1 className="text-3xl sm:text-2xl font-bold mb-4 mt-2 text-center text-slate-100">
+    <div className="lg:w-[1024px] xl:w-[1280px] w-full mx-auto relative mb-10 mt-10">
+      <div className="bg-pdark-grey inline-block w-full h-full align-top mx-auto px-10 py-10">
+        <h1 className="text-3xl md:text-2xl font-bold mb-4 mt-2 text-center text-slate-100">
           {local.shop_name}
         </h1>
       </div>
-      <div className="flex flex-row justify-evenly  mt-10">
-        <div className="w-[50%] flex flex-col  items-center">
-          <div className="h-[600px] w-[600px]">
+      <div className="flex flex-col md:flex-row justify-center md:justify-evenly  mt-10">
+        <div className="w-full md:w-1/2  flex flex-col items-center">
+          <div className="h-60 md:h-96 w-60 md:w-96">
             <img
               className="mx-auto h-full rounded-md"
               src={local.image}
@@ -133,7 +133,7 @@ function Local() {
             />
           </div>
           <Star paramRating={local.rating}></Star>
-          <div className="mt-10 w-[90%] ">
+          <div className="mt-10 w-11/12 md:w-5/6 ">
             <Tabs localId={id} />
             <div className="flex gap-3">
               <Button onClick={() => handleOpenModal('Comentarios')}>
@@ -145,14 +145,14 @@ function Local() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col w-[600px] ">
+        <div className="flex flex-col w-full md:w-1/2 mt-5 md:mt-0 ">
           <div
-            className=" bg-pdark-grey h-[600px] p-1 rounded-md  w-full flex flex-col"
+            className="bg-pdark-grey h-60 md:h-96 p-1 rounded-md w-full md:w-5/6 ml-auto  flex flex-col"
             id="chat"
           >
             <Chat local={local.shop_id.toString()} />
           </div>
-          <div className=" w-[400px] mt-10 bg-pdark-grey p-10  rounded-md mx-auto border text-center">
+          <div className="w-full c mt-10 bg-pdark-grey p-10 rounded-md mx-auto md:w-1/2 text-center">
             <Link to="/">
               <h1 className="text-slate-100">URL DEL LOCAL</h1>
             </Link>
