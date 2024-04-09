754f2b279306369dd229bbfe9b26af70d91e868e
Javier Figueroa <javiernfigueroa@gmail.com>
2024-04-09 16:41:02 -0400

    fix tabs

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -1,30 +1,26 @@
 import { useState, useEffect } from 'react';
-import Modal from './Modal.jsx';
 import { useForm } from 'react-hook-form';
 import { ENDPOINT } from '../config/constans.js';
 import axios from 'axios';
-//coment/
+
 function Tabs({ localId, view }) {
   const {
     register,
     handleSubmit,
     formState: { errors },
+    reset,
   } = useForm();
+
   const [activeTabIndex, setActiveTabIndex] = useState(0);
   const [consumedData, setConsumedData] = useState();
   const [reviewData, setReviewData] = useState();
-  const [showModal, setShowModal] = useState(false);
-  const [modalContent, setModalContent] = useState(null);
+  const [showForm, setShowForm] = useState(false);
+  const [formType, setFormType] = useState('');
 
-  const handleOpenModal = (content) => {
-    setModalContent(content);
-    setShowModal(true);
-  };
   const fetchData = async () => {
     try {
       const response = await axios.get(`${ENDPOINT.consumed}/${localId}`);
       setConsumedData(response.data.consumed);
-      //console.log(consumedData)
 
       const resReview = await axios.get(`${ENDPOINT.reviews}/${localId}`);
       setReviewData(resReview.data.reviews);
@@ -32,15 +28,27 @@ function Tabs({ localId, view }) {
       console.error('Error fetching data:', error);
     }
   };
+
   useEffect(() => {
     fetchData();
-  }, [0]);
+  }, [localId]);
+
   const isSubmit = (data) => {
-    if (data.comment === undefined) {
-      sendPrice(data);
-    } else {
+    if (formType === 'comment') {
       sendComment(data);
+    } else if (formType === 'price') {
+      sendPrice(data);
     }
+  };
+
+  const handleOpenForm = (type) => {
+    setFormType(type);
+    setShowForm(true);
+  };
+
+  const handleCloseForm = () => {
+    setShowForm(false);
+    reset();
   };
 
   const sendComment = async (data) => {
@@ -54,23 +62,20 @@ function Tabs({ localId, view }) {
         calification: null,
       };
 
-      // Configurar el encabezado de la solicitud con el token
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
 
-      // Realizar la solicitud POST al backend con el encabezado configurado
       await axios.post(ENDPOINT.reviews, comentario, config);
 
-      // Opcional: Puedes manejar alguna acción después de enviar los comentarios, como cerrar el modal
-      setShowModal(false);
+      setShowForm(false);
+      reset();
+      fetchData();
     } catch (error) {
       console.error('Error al enviar comentario:', error);
-      // Manejar el error según tus necesidades
     }
-    fetchData();
   };
 
   const sendPrice = async (data) => {
@@ -84,25 +89,20 @@ function Tabs({ localId, view }) {
         price_paid: parseFloat(data.price_paid),
       };
 
-      console.log(producto);
-
-      // Configurar el encabezado de la solicitud con el token
       const config = {
         headers: {
           Authorization: `Bearer ${token}`,
         },
       };
 
-      // Realizar la solicitud POST al backend con el encabezado configurado
       await axios.post(ENDPOINT.consumed, producto, config);
 
-      // Opcional: Puedes manejar alguna acción después de enviar los comentarios, como cerrar el modal
-      setShowModal(false);
+      setShowForm(false);
+      reset();
+      fetchData();
     } catch (error) {
-      console.error('Error al enviar comentario:', error);
-      // Manejar el error según tus necesidades
+      console.error('Error al enviar precio:', error);
     }
-    fetchData();
   };
 
   const tabsData = [
@@ -115,7 +115,7 @@ function Tabs({ localId, view }) {
                 {`${consumed.product.toUpperCase()}: $${parseFloat(consumed.price_paid).toFixed(0)}`}
               </li>
             ))
-          : 'No se agregado precios',
+          : 'No se han agregado precios',
     },
     {
       label: 'Comentarios',
@@ -133,21 +133,19 @@ function Tabs({ localId, view }) {
   return (
     <div className="ml-5 text-white">
       <div className="flex gap-3 ml-4 ">
-        {tabsData.map((tab, idx) => {
-          return (
-            <button
-              key={idx}
-              className={`py-2 border-b-4 transition-colors duration-300 ${
-                idx === activeTabIndex
-                  ? 'border-porange'
-                  : 'border-transparent hover:border-porange'
-              }`}
-              onClick={() => setActiveTabIndex(idx)}
-            >
-              {tab.label}
-            </button>
-          );
-        })}
+        {tabsData.map((tab, idx) => (
+          <button
+            key={idx}
+            className={`py-2 border-b-4 transition-colors duration-300 ${
+              idx === activeTabIndex
+                ? 'border-porange'
+                : 'border-transparent hover:border-porange'
+            }`}
+            onClick={() => setActiveTabIndex(idx)}
+          >
+            {tab.label}
+          </button>
+        ))}
       </div>
       <div className="py-4 ml-4">
         {view !== 'gallery' ? (
@@ -159,95 +157,112 @@ function Tabs({ localId, view }) {
             {tabsData[activeTabIndex].content || 'Loading...'}
           </ul>
         )}
-        {view !== 'gallery' ? (
-          tabsData[activeTabIndex].label === 'Precios' ? (
-            <button
-              className="bg-porange p-2 rounded-sm mt-4"
-              onClick={() => handleOpenModal('Precios')}
-            >
-              Agregar
-            </button>
-          ) : (
-            <button
-              className="bg-porange p-2 rounded-sm mt-4"
-              onClick={() => handleOpenModal('Comentarios')}
-            >
-              Comentar
-            </button>
-          )
-        ) : null}
+        {view !== 'gallery' && (
+          <button
+            className="bg-porange p-2 rounded-sm mt-4"
+            onClick={() =>
+              handleOpenForm(
+                tabsData[activeTabIndex].label === 'Precios'
+                  ? 'price'
+                  : 'comment',
+              )
+            }
+          >
+            {tabsData[activeTabIndex].label === 'Precios'
+              ? 'Agregar precio'
+              : 'Comentar'}
+          </button>
+        )}
       </div>
-      {showModal && (
-        <Modal setShowModal={setShowModal}>
-          {modalContent === 'Comentarios' ? (
-            <>
-              <form
-                onSubmit={handleSubmit(isSubmit)}
-                className="flex flex-col gap-5"
-              >
-                <h3 className="text-white p-0 m-0">Deja tu comentario</h3>
-                <input
-                  className="p-1 text-black"
-                  defaultValue=""
-                  placeholder="Ingresa tu comentario"
-                  {...register('comment', { required: true })}
-                />
-                {errors.comment && (
-                  <span className="text-white bg-red-500 p-2">
-                    Debes ingresar un comentario
-                  </span>
-                )}
-                {/* Verificar si el usuario está logueado antes de mostrar el botón de enviar */}
-                {localStorage.getItem('id') && localStorage.getItem('token') ? (
-                  <input type="submit" className=" bg-porange rounded-lg" />
-                ) : (
-                  <span className="text-white bg-red-500 p-2">
-                    Para enviar un comentario debes estar logueado.
-                  </span>
-                )}
-              </form>
-            </>
-          ) : (
-            <>
-              <form
-                onSubmit={handleSubmit(isSubmit)}
-                className="flex flex-col gap-5"
-              >
-                <h3 className="text-white p-0 m-0">Crea tu producto</h3>
-                <input
-                  className="p-1 text-black"
-                  defaultValue=""
-                  placeholder="Ingresa tu producto"
-                  {...register('product', { required: true })}
-                />
-                {errors.product && (
-                  <span className="text-white bg-red-500 p-2">
-                    Debes ingresar un producto
-                  </span>
-                )}
-                <input
-                  className="p-1 text-black"
-                  defaultValue=""
-                  placeholder="Ingresa el precio"
-                  {...register('price_paid', { required: true })}
-                />
-                {errors.price_paid && (
-                  <span className="text-white bg-red-500 p-2">
-                    Debes ingresar un precio
-                  </span>
-                )}
-                {/* Verificar si el usuario está logueado antes de mostrar el botón de enviar */}
-                {localStorage.getItem('id') && localStorage.getItem('token') ? (
-                  <input type="submit" className=" bg-porange rounded-lg" />
-                ) : (
-                  <span className="text-white bg-red-500 p-2">
-                    Para agregar un producto debes estar logueado.
-                  </span>
-                )}
-              </form>
-            </>
-          )}
-        </Modal>
+      {showForm && (
+        <div className="absolute w-[500px] h-[400px] z-10 bg-transparent top-1/4 left-1/3">
+          <div className="h-full bg-pgrey rounded-xl">
+            <div className="flex justify-between p-5">
+              <h3 className="text-white">
+                Hola {localStorage.getItem('nickname')}{' '}
+              </h3>
+              <button className="text-white" onClick={handleCloseForm}>
+                X
+              </button>
+            </div>
+            <div className="flex flex-col p-5">
+              {formType === 'price' ? (
+                <form
+                  onSubmit={handleSubmit(isSubmit)}
+                  className="flex flex-col gap-5"
+                >
+                  <h3 className="text-white p-0 m-0">Agregar precio</h3>
+                  <input
+                    className="p-1 text-black"
+                    defaultValue=""
+                    placeholder="Ingresa tu producto"
+                    {...register('product', { required: true })}
+                  />
+                  {errors.product && (
+                    <span className="text-white bg-red-500 p-2">
+                      Debes ingresar un producto
+                    </span>
+                  )}
+                  <input
+                    className="p-1 text-black"
+                    defaultValue=""
+                    placeholder="Ingresa el precio"
+                    {...register('price_paid', {
+                      required: true,
+                      validate: {
+                        mustBeNumber: (value) => !isNaN(parseFloat(value)),
+                      },
+                    })}
+                  />
+                  {errors.price_paid?.type === 'required' && (
+                    <span className="text-white bg-red-500 p-2">
+                      Debes ingresar un precio
+                    </span>
+                  )}
+                  {errors.price_paid?.type === 'mustBeNumber' && (
+                    <span className="text-white bg-red-500 p-2">
+                      El precio debe ser un número
+                    </span>
+                  )}
+                  {localStorage.getItem('id') &&
+                  localStorage.getItem('token') ? (
+                    <input type="submit" className=" bg-porange rounded-lg" />
+                  ) : (
+                    <span className="text-white bg-red-500 p-2">
+                      Para agregar un producto debes estar logueado.
+                    </span>
+                  )}
+                </form>
+              ) : (
+                <form
+                  onSubmit={handleSubmit(isSubmit)}
+                  className="flex flex-col gap-5"
+                >
+                  <h3 className="text-white p-0 m-0">Deja tu comentario</h3>
+                  <input
+                    className="p-1 text-black"
+                    defaultValue=""
+                    placeholder="Ingresa tu comentario"
+                    {...register('comment', { required: true })}
+                  />
+                  {errors.comment && (
+                    <span className="text-white bg-red-500 p-2">
+                      Debes ingresar un comentario
+                    </span>
+                  )}
+                  {localStorage.getItem('id') &&
+                  localStorage.getItem('token') ? (
+                    <input type="submit" className=" bg-porange rounded-lg" />
+                  ) : (
+                    <span className="text-white bg-red-500 p-2">
+                      Para enviar un comentario debes estar logueado.
+                    </span>
+                  )}
+                </form>
+              )}
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
