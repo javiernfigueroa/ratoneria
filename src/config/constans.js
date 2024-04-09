7550767c09f9b88866a6e4a8863008ce1dccc522
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-04-08 22:00:04 -0400

    fix: routes users

--- src/config/constans.js
+++ src/config/constans.js
@@ -2,9 +2,9 @@ export const URLBASE = 'http://localhost:3000/api/v1';
 
 export const ENDPOINT = {
   login: `${URLBASE}/login`,
-  users: `${URLBASE}/usuarios`,
+  users: `${URLBASE}/users`,
   reviews: `${URLBASE}/reviews`,
   shops: `${URLBASE}/shops`,
   consumed: `${URLBASE}/consumed`,
-  auth_user: `${URLBASE}/auth_user`
+  auth_user: `${URLBASE}/auth_user`,
 };
