d5bbc329d6196c2b7c7ff90d88cad6b7ab3aaa50
Javier Figueroa <javiernfigueroa@gmail.com>
2024-04-10 18:52:16 -0400

    fix useAsk

--- src/hooks/useAskQuestions.js
+++ src/hooks/useAskQuestions.js
@@ -1,29 +1,23 @@
-import { useState } from 'react';
-
+import { useState } from "react";
 import axios from 'axios';
 
 const useAskQuestion = () => {
-    const [answer, setAnswer] = useState(null);
-    const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+  const [answer, setAnswer ] = useState(null);
+  const [error, setError ] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-    const askQuestion = async (question) => {
-        console.log('data', {question})
-      setLoading(true);
-      try {
-        const response = await axios({
-          method: 'get',
-          url: `${import.meta.env.VITE_BACKEND_URL}/ask`,
-          data: { 'question': question }
-        });
-        setAnswer(response.data.answer);
-      } catch (error) {
-        setError(error.message);
-      } finally {
-        setLoading(false);
-      }
-    };
-    return { answer, error, loading, askQuestion };
+  const askQuestion = async (question) => {
+    setLoading(true);
+    try{
+      const response = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/ask?question=${encodeURIComponent(question)}`);
+      setAnswer(response.data.answer)
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
   };
+  return {answer, error, loading, askQuestion}
+};
 
-  export default useAskQuestion;
+export default useAskQuestion;
