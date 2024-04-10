09cba733f5a6a975434ebe95b4dab63662ed58f4
TomasMunozC <tomasmunozc@gmail.com>
2024-04-09 22:46:58 -0400

    fix starts

--- src/components/Star.jsx
+++ src/components/Star.jsx
@@ -1,21 +1,28 @@
 import { useState, useEffect } from 'react';
 import { FaStar } from 'react-icons/fa';
-import axios from 'axios'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
-
-
-function Star({ paramRating,localId }) {
+function Star({ paramRating, localId, enableHover }) {
   const [rating, setRating] = useState(paramRating);
   const [hover, setHover] = useState(null);
   const [isCalificated, setIsCalificated] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+  const navigate = useNavigate();
 
-  const compareUser = async () =>{
+  const handleCloseAlert = () => {
+    setErrorMessage('');
+  };
+
+  const compareUser = async () => {
     const userId = localStorage.getItem('id');
     const response = await axios.get(
-      `http://localhost:3000/api/v1/reviews_user_calification?shop=${localId}&user=${userId}`)
-      if (response.data !== ""){
-        setIsCalificated(true)}
-  }
+      `http://localhost:3000/api/v1/reviews_user_calification?shop=${localId}&user=${userId}`
+    );
+    if (response.data !== '' ){
+      setIsCalificated(true);
+    }
+  };
 
   useEffect(() => {
     setRating(paramRating);
@@ -41,26 +48,45 @@ function Star({ paramRating,localId }) {
       };
 
       // Realizar la solicitud POST al backend con el encabezado configurado
-      await axios.post(
-        'http://localhost:3000/api/v1/reviews',
-        data,
-        config,
-      );
-
+      await axios.post('http://localhost:3000/api/v1/reviews', data, config);
+      setIsCalificated(true);
     } catch (error) {
-      console.error('Error al enviar comentario:', error);
+      if(error.response.data.error==="el token no es valido"){
+        if(localStorage.getItem("id")){
+          localStorage.clear();
+          navigate("/login")
+        }
+        localStorage.clear();
+        setErrorMessage("Debes iniciar sesion para calificar");
+      }
       // Manejar el error según tus necesidades
     }
   };
 
   const handleClick = (newRating) => {
-    if(!isCalificated){
-    setRating(newRating);
-    sendCalification(newRating);
-  }};
+    if (!isCalificated && enableHover === true) {
+      setRating(newRating);
+      sendCalification(newRating);
+    }
+  };
 
   return (
     <div className="flex flex-row justify-center mb-3">
+            <div className="fixed top-0 left-0 right-0 z-50 mt-4">
+        {errorMessage && (
+          <div className="mx-auto w-1/3 bg-gray-500 bg-opacity-70 text-white font-bold p-2 rounded-md shadow-md">
+            <div className="text-center mb-4">{errorMessage}</div>
+            <div className="flex justify-center mt-2">
+              <button
+                onClick={handleCloseAlert}
+                className="bg-porange px-4 py-1 rounded-lg text-white"
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        )}
+      </div>
       {[...Array(5)].map((_, index) => {
         const currentRating = index + 1;
         return (
@@ -75,9 +101,13 @@ function Star({ paramRating,localId }) {
             <FaStar
               className="cursor-pointer"
               size={35}
-              color={currentRating <= (hover || rating) ? '#ffa31a' : '#808080'}
-              onMouseEnter={() => setHover(currentRating)}
-              onMouseLeave={() => setHover(null)}
+              color={
+                currentRating <= (enableHover ? hover||rating : rating)
+                  ? '#ffa31a'
+                  : '#808080'
+              }
+              onMouseEnter={() => enableHover && setHover(currentRating)}
+              onMouseLeave={() => enableHover && setHover(null)}
             />
           </label>
         );
