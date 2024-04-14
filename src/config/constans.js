8d3e5e7aff4f2cd9a568146aa35929d844daedce
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-04-14 00:08:19 -0400

    fix: login

--- src/config/constans.js
+++ src/config/constans.js
@@ -1,7 +1,8 @@
-export const URLBASE = import.meta.env.VITE_BACKEND_URL ||'http://localhost:3000/api/v1';
+export const URLBASE =
+  import.meta.env.VITE_BACKEND_URL || 'http://localhost:3000/api/v1';
 
 export const ENDPOINT = {
-  chat: 'backchat-production-8699.up.railway.app',
+  chat: 'back-ratoneria-production.up.railway.app',
   login: `${URLBASE}/login`,
   users: `${URLBASE}/users`,
   reviews: `${URLBASE}/reviews`,
@@ -9,4 +10,6 @@ export const ENDPOINT = {
   shops_paginated: `${URLBASE}/shops_whit_pagination`,
   consumed: `${URLBASE}/consumed`,
   auth_user: `${URLBASE}/auth_user`,
+  auth_google: `${URLBASE}/auth_google`,
+  register_google: `${URLBASE}/register_google`,
 };
