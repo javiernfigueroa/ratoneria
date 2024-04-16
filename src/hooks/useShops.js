2fdd9df6448cc40720f9c75c657c54fabc0a0a5e
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-04-16 12:06:29 -0400

    add: getShopAnswer() in Searchbar.jsx

--- src/hooks/useShops.js
+++ src/hooks/useShops.js
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react';
 import axios from 'axios';
 import { ENDPOINT } from '../config/constans';
 
-const useGetShops = () => {
+const useGetShops = (data) => {
   const [shops, setShops] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -10,8 +10,12 @@ const useGetShops = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(ENDPOINT.shops);
-        setShops(response.data);
+        if (data) {
+          setShops(data);
+        } else {
+          const response = await axios.get(ENDPOINT.shops);
+          setShops(response.data);
+        }
         setLoading(false);
       } catch (error) {
         console.error('Error en la solicitud:', error);
