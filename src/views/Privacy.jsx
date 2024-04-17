e1d3bf42bd413ec5285caf9a28895584812ed6d0
Renato Francisco Báez Lobos <panchobaez@gmail.com>
2024-04-16 23:00:15 -0400

    feat: terms and privacy

---
+++ src/views/Privacy.jsx
@@ -0,0 +1,26 @@
+
+
+function Privacy() {  
+  return (
+    <div className="mx-auto relative mb-10 mt-10 max-w-[1280px]">
+      <h1 className="text-slate-100 text-3xl sm:text-2xl md:text-2xl xl:text-5xl lg:text-5xl font-bold mb-2 mt-2 mx-4">
+        Politica de privacidad
+      </h1> 
+      <section className="text-slate-100 mx-10 relative mb-10 mt-4">
+        <h2>Información recopilada</h2>
+        <p>En nuestra aplicación, recopilamos cierta información personal y no personal de nuestros usuarios.</p>
+        <h2>Uso de la información</h2>
+        <p>La información recopilada se utiliza para mejorar la experiencia del usuario y proporcionar servicios personalizados.</p>
+        <h2>Divulgación a terceros</h2>
+        <p>No compartimos información personal con terceros, excepto cuando sea necesario para proporcionar servicios o cumplir con la ley.</p>
+        <h2>Seguridad</h2>
+        <p>Tomamos medidas de seguridad para proteger la información personal de nuestros usuarios contra acceso no autorizado o alteración.</p>
+        <h2>Consentimiento</h2>
+        <p>Al utilizar nuestra aplicación, aceptas nuestra política de privacidad y el tratamiento de tu información según lo descrito en este documento.</p>
+        <h2>Actualizaciones</h2>
+        <p>Nos reservamos el derecho de actualizar o cambiar nuestra política de privacidad en cualquier momento. Se notificarán los cambios relevantes a través de la aplicación o nuestro sitio web.</p>
+      </section>
+    </div>
+  );
+}
+export default Privacy;
